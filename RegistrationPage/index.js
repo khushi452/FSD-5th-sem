b6271312c8baa5fc9f62af564b3482177c0bfbfe
khushi452 <khushisharmaclass10@gmail.com>
2026-07-24 10:07:51 +0530

    day 1

---
+++ RegistrationPage/index.js
@@ -0,0 +1,5 @@
+const email = documrent.getElementById('email');
+const name = document.getElementById('name');
+const password = document.getElementById("password");
+const btn = document.getElementById("btn");
+ 
